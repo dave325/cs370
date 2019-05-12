93dd4b8c0f4ef96e7c1d4c817e5b12fdac06d73c
czcz7890 <dongsoochung99@gmail.com>
2019-05-12 17:14:23 -0400

    file for parsers

--- parsers/bonnet_case_load.js
+++ parsers/bonnet_case_load.js
@@ -6,26 +6,23 @@ request({method: 'GET',url: URL},
     (err, res, body) => {
         if (err) return console.error(err);
         
-        let object = {}
         let $ = cheerio.load(body);
-
+        let object = {};
         object["form-action"] = $("link").attr('href');
         
         object["title"] = $("title").text();
         
         object['body-background'] = $('body').attr('background');
-    
-        $('tr').each(function(i, e) {
-            if($(this).text().trim() == "Username:"){
-                object["username_name"] = $(this).find('input').attr('name');
-            }else if($(this).text().trim() == "Password:"){
-                object["password_name"] = $(this).find('input').attr('name');
+
+        $('input').each(function(index, e){
+            if($(this).attr('name')){
+                object[$(this).attr('name')] = $(this).attr('value');
             }else{
-                
-                object["submit_value"] = $(this).find('input').attr('value');
+                object['submit'] = $(this).attr('value');
             }
+            
         })
-        
+
         //console.log(JSON.stringify(object));
         return object;
 });
