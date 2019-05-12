58a03ca951c0d6c535423a7fd02641ee9f6ee125
Deepak Khemraj <drk3931@gmail.com>
2019-05-12 19:17:52 -0400

    committing before home page fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -16,6 +16,10 @@ const routes: Routes = [
     loadChildren: './list/list.module#ListPageModule'
   },
   {
+    path:'home',
+    loadChildren: './home/home.module#HomePageModule'
+  },
+  {
     path:'about',
     component: AboutComponent
   },
