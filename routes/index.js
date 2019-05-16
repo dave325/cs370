f478b901450486861a1f6e0cff2331953c22839f
David <davedataram@gmail.com>
2019-05-16 06:58:50 -0400

    Fixed login request

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var http = require('http');
 
 var  bonnett_port = 7778;
-var  bonnett_host = 'http://bonnet19.cs.qc.cuny.edu';
+var  bonnett_host = '/bonnet19.cs.qc.cuny.edu';
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
