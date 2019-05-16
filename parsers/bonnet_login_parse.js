6264515f99429acee68b08204e98bf04d381f342
czcz7890 <dongsoochung99@gmail.com>
2019-05-15 21:32:50 -0400

    new login parser

--- parsers/bonnet_login_parse.js
+++ parsers/bonnet_login_parse.js
@@ -3,31 +3,28 @@ var  request = require('request');
 var  URL = "http://bonnet19.cs.qc.edu:7778/pls/forum/ec_forum.access_check";
 
 module.exports.login = (req, response) => {
-    request({ method: 'GET', url: URL },
-        (err, res, body) => {
-            if (err) return console.error(err);
+    request({ method: 'POST', url: URL, form: { p_usr_username: req.body.p_usr_username, p_usr_password: req.body.p_usr_password} },
+    (err, res, body) => {
+        if (err) return console.error(err);
 
-            var object = {}
-            var $ = cheerio.load(body);
+        var $ = cheerio.load(body);
 
-            object["form-action"] = $("link").attr('href');
-
-            object["title"] = $("title").text();
-
-            object['body-background'] = $('body').attr('background');
-
-            $('tr').each(function (i, e) {
-                if ($(this).text().trim() == "Username:") {
-                    object["username_name"] = $(this).find('input').attr('name');
-                } else if ($(this).text().trim() == "Password:") {
-                    object["password_name"] = $(this).find('input').attr('name');
-                } else {
-
-                    object["submit_value"] = $(this).find('input').attr('value');
+        var object = {};
+        
+        $('INPUT').each(function (i, e) {
+            
+            if ($(this).attr('name')) {
+                
+                object[$(this).attr('name')] = $(this).attr('value');
+                //console.log($(this).attr('value'));
+                if ($(this).attr('NAME') == "p_community") {
+                    object["p_community"] = $(this).attr('value');
+                } else if ($(this).attr('NAME') == "p_session") {
+                    object["p_session"] = $(this).attr('value');
                 }
-            })
-
-            //console.log(JSON.stringify(object));
-            return object;
-        });
+            }
+        })
+        console.log(JSON.stringify(object));
+        return object;
+    });
 }
