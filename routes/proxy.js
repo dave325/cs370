4d1c9c485022d5d3db8264d8e2144f005dba625b
czcz7890 <dongsoochung99@gmail.com>
2019-05-15 23:17:48 -0400

    proxy login collision

--- routes/proxy.js
+++ routes/proxy.js
@@ -21,47 +21,7 @@ var  CommunityUserList = require('../parsers/List_of_users_in_community');
 
 var  bonnett_port = 7778;
 var  bonnett_host = 'bonnet19.cs.qc.cuny.edu';
-router.post("/login",
 
-  (req, res) => {
-
-    console.log("LOGIN REQUEST RECIEVED");
-
-
-    var postData = "p_usr_username=" + req.body.p_usr_username + "&" + "p_usr_password=" + req.body.p_usr_password;
-
-    console.log("The Query String is: " + postData);
-    var options = {
-      host: bonnett_host,
-      port: bonnett_port,
-      path: '/pls/forum/ec_forum.access_check',
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        'Content-Length': Buffer.byteLength(postData)
-      }
-    };
-
-    var reqToBonnett = http.request(options,
-      (response) => {
-        htmlResponse = "";
-        htmlResponse += "<h1>THIS IS THE RETURNED RESPONSE FROM DEEPAK'S SERVER</h1>";
-
-        response.setEncoding('utf8');
-        response.on('data', function (chunk) {
-          htmlResponse += chunk;
-        });
-        response.on('end', function () {
-          res.send(htmlResponse);
-        })
-      }
-
-    );
-
-    reqToBonnett.write(postData);
-    reqToBonnett.end();
-
-  });
 router.post('/case', caseReport.caseCreate);
 router.post('/caseload', caseLoad.caseLoad);
 router.post('/caseCreate', caseCreate.caseCreate);
