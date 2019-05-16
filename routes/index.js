8251a70833a4ef032d593d061f84befb567a7d3b
David <davedataram@gmail.com>
2019-05-16 09:29:12 -0400

    Added login to proxy route file

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var router = express.Router();
 var http = require('http');
-
+var path = require('path');
 var  bonnett_port = 7778;
 var  bonnett_host = '/bonnet19.cs.qc.cuny.edu';
 /* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('**', function (req, res, next) {
+  res.sendFile(path.join(__dirname, "/www/index.html"));
 });
 
 module.exports = router;
