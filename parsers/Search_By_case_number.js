5560f5e25ed25614f1789e7914c11b4afbc9de8e
David <davedataram@gmail.com>
2019-05-16 07:45:20 -0400

    Fixed case number request

--- parsers/Search_By_case_number.js
+++ parsers/Search_By_case_number.js
@@ -20,7 +20,16 @@ var json = [];
 
 module.exports.SearchResult = (req, res) => {
     //using 'List of threaded cases posted in your (selected Special Interest Groups and) community'
-    request(options, (error, response, body) => {
+    request({
+        url: "http://bonnet19.cs.qc.cuny.edu:7778/pls/forum/ec_forum.find_case",
+        method: "POST",
+       // path: '/pls/forum/ec_forum.find_cases_by_name',
+        form: {
+            p_case_select: req.body.case_id,
+            p_session: req.body.p_session_id,
+            p_community_id: req.body.p_community_id
+        }
+    }, (error, response, body) => {
         if (error) {
             console.log(err);
         } else {
