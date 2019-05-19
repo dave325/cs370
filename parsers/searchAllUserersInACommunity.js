7b196b33a36716401d5d41bb65906fe2fe50a805
David <davedataram@gmail.com>
2019-05-19 12:57:22 -0400

    ALlow users to successfully download files from server

--- parsers/searchAllUserersInACommunity.js
+++ parsers/searchAllUserersInACommunity.js
@@ -17,7 +17,7 @@ module.exports.list = (req, res) => {
         url: "http://bonnet19.cs.qc.cuny.edu:7778/pls/forum/ec_admin.show_users_dispatch",
         method: "POST",
         form: {
-            p_usr_username: "dave325",
+            p_usr_username: "david325",
             p_usr_password: "d32594"
         }
     }, (error, response, body) => {
