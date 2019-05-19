247ed73b0edb3327fec35a8c33bee599ab77e984
David <davedataram@gmail.com>
2019-05-18 22:33:45 -0400

    Alterign search result final link

--- parsers/Search_result_final_link.js
+++ parsers/Search_result_final_link.js
@@ -96,7 +96,7 @@ module.exports.SearchResultFileLink = (req, res) => {
         } else {
             console.log(formOptions)
             if (formOptions.p_attachOrReply === "Get-attachment") {
-                res.attachment("http://bonnet19.cs.qc.cuny.edu:7778/EC_dropoff/4849ole16m4y19547.pptx").status(file.statusCode);
+                res.attachment("http://bonnet19.cs.qc.cuny.edu:7778/EC_dropoff/4849ole16m4y19547.pptx").status(200);
 
                 return;
             }
