ff452184ab7aacb32d7d2ca0b5102e815c14b3bf
czcz7890 <dongsoochung99@gmail.com>
2019-05-18 23:44:31 -0400

    uploading file

--- parsers/bonnet_case_load_parse.js
+++ parsers/bonnet_case_load_parse.js
@@ -1,10 +1,11 @@
 var  cheerio = require('cheerio');
 var  request = require('request');
-var  URL = "http://localhost:8080";
+var  URL = "http://bonnet19.cs.qc.edu:7778/pls/forum/EC_forum.add_attachment";
 
-module.exports.caseLoad = (req, response) => {
-    request({ method: 'GET', url: URL },
-        (err, res, body) => {
+module.exports.caseLoad = (req, res) => {
+    request({ method: 'GET', url: URL, form: {p_ses: req.body.p_ses, p_sig: req.body.p_sig, p_subject: req.body.p_subject,
+        p_keyword: req.body.p_keyword, p_url: req.body.p_url, p_text: req.body.p_text}},//p_sig: 209 to 215
+        (err, response, body) => {
             if (err) return console.error(err);
 
             var $ = cheerio.load(body);
@@ -25,6 +26,7 @@ module.exports.caseLoad = (req, response) => {
             })
 
             //console.log(JSON.stringify(object));
-            return object;
+            res.json(object).status(200);
+            
         });
 }
