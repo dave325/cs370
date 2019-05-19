a15f4747f5f93f1f817db7b0313f4ca2579977da
David <davedataram@gmail.com>
2019-05-18 23:30:50 -0400

    Alterign search result final link

--- parsers/Search_result_final_link.js
+++ parsers/Search_result_final_link.js
@@ -111,6 +111,7 @@ module.exports.SearchResultFileLink = (req, res) => {
                         file.pipe(temp);
                         temp.on('finish', function () {
                             temp.close(function(){
+                                res.set('Content-disposition', 'attachment; filename=test.pptx')
                                 res.download(temp);
                             });  // close() is async, call cb after close completes.
                         });
