03c060267350d64dc0085628000fb46c31bc4c8d
czcz7890 <dongsoochung99@gmail.com>
2019-05-19 00:08:48 -0400

    missing input field

--- parsers/bonnet_create_case_parse.js
+++ parsers/bonnet_create_case_parse.js
@@ -3,7 +3,8 @@ var  request = require('request');
 var  URL = "http://bonnet19.cs.qc.edu:7778/pls/forum/EC_forum.send_interface";
 
 module.exports.caseCreate = (req, res) => {
-    request({ method: 'POST', url: URL, form: {p_usr_username: req.body.p_usr_username, p_usr_password: req.body.p_usr_password, p_ses: req.body.p_ses}},
+    request({ method: 'POST', url: URL, form: {p_usr_username: req.body.p_usr_username, p_usr_password: req.body.p_usr_password, p_ses: req.body.p_ses,
+        p_community_id:req.body.p_community_id}},
         (err, response, body) => {
             if (err) return console.error(error);
 
