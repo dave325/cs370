72db08fc606489ae9f65e80ac07988e161251950
David <davedataram@gmail.com>
2019-05-21 07:10:24 -0400

    Fix id issue

--- parsers/Search_result_final_link.js
+++ parsers/Search_result_final_link.js
@@ -96,7 +96,7 @@ module.exports.SearchResultFileLink = (req, res) => {
                         //var stat = fs.statSync("http://bonnet19.cs.qc.cuny.edu:7778/EC_dropoff/4849ole16m4y19547.pptx");
                         //var fileToSend = fs.readFileSync("http://bonnet19.cs.qc.cuny.edu:7778/EC_dropoff/4849ole16m4y19547.pptx");
                         res.set('Content-Type', file.headers['content-type']);
-                        res.setHeader('Content-disposition', 'attachment; filename=' + fileName);
+                        res.set('Content-disposition', 'attachment; filename=' + fileName);
                         file.pipe(temp);
                         temp.on('finish', function () {
                             temp.close(function () {
