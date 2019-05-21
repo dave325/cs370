8e6045a967ca2647e62253f30b377104bf4a008b
Deepak Khemraj <drk3931@gmail.com>
2019-05-21 02:17:31 -0400

    added buttons to the frontend

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalController } from '@ionic/angular';
 import { LoginComponent } from '../login/login.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,12 +11,38 @@ import { LoginComponent } from '../login/login.component';
 export class HomePage {
 
   constructor(
-    private modal: ModalController
+    private modal: ModalController,private router:Router
   ) {}
   async sub(){
     var  modal = await this.modal.create({
       component: LoginComponent
     });
     return await modal.present();
+  }
+
+  toAbout()
+  {
+    this.router.navigateByUrl('about')
+  }
+
+  toView()
+  {
+    this.router.navigateByUrl('list')
+  }
+
+  toUpload()
+  {
+    this.router.navigateByUrl('upload')
+  }
+
+  toRegister()
+  {
+    this.router.navigateByUrl('register')
+  }
+
+
+  toUsers()
+  {
+    this.router.navigateByUrl('listUsers')
   }
 }
