e1695433cac6b3aa6c9d166683e721052447d05f
HossainNazmul <nazmul789@gmail.com>
2019-05-21 07:46:24 -0400

    fix

--- parsers/Search_By_date.js
+++ parsers/Search_By_date.js
@@ -4,13 +4,14 @@ var  request = require('request')
 var json = [];
 
 module.exports.SearchResult = (req, res) => {
-    console.log(req.body.p_increment_date +  '\n' + req.body.p_from_date +  '\n' + req.body.p_to_date +  '\n' + req.body.p_session_id+  '\n' + req.body.p_community_id)
+    
+    //console.log(req.body.p_increment_date +  '\n' + req.body.p_from_date +  '\n' + req.body.p_to_date +  '\n' + req.body.p_session_id+  '\n' + req.body.p_community_id)
 
     request({
         url: "http://bonnet19.cs.qc.cuny.edu:7778/pls/forum/ec_forum.find_cases_by_date",
         method: "POST",
         form: {
-            p_increment_date: "ALL_DAY",
+            p_increment_date: req.body.p_increment_date,
             p_from_date: req.body.p_from_date,
             p_to_date: req.body.p_to_date,
             p_session: req.body.p_session_id,
@@ -36,15 +37,13 @@ module.exports.SearchResult = (req, res) => {
                     var date = $(this).children('td:nth-child(2)').text().trim();
                     var author = $(this).children('td:nth-child(3)').text().trim();
                     var subject = $(this).children('td:nth-child(4)').text().trim();
-                    if( author.trim().length != 0){
-                        var obj = {
-                            "GetCase" : getCase_value,
-                            "Date" : date,
-                            "Author" : author,
-                            "Subject" : subject
-                        };
-                    }
-                        
+                    
+                    var obj = {
+                        "GetCase" : getCase_value,
+                        "Date" : date,
+                        "Author" : author,
+                        "Subject" : subject
+                    };
         
                     data.push(obj);
                 }
